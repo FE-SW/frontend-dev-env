af3097255e618ec42a7e331800c6f3deeacfecf6
FE-SW <ingal777@naver.com>
2023-01-18 07:22:54 +0900

    Feat: babel.config.js, babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,12 @@ module.exports = {
       }
       /**
        * TODO: babel-loader를 구성해 보세요.
-       */
+       */,
+       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader", // 바벨을 웹팩으로 통합해서 사용하게 해주는 웹팩 플러그인
+      },
     ]
   },
   plugins: [
@@ -64,3 +69,18 @@ module.exports = {
       : [])
   ]
 };
+
+// babel-loader:바벨을 웹팩으로 통합해서 사용하게 해주는 웹팩 플러그인
+// core-js:  폴리필 라이브러리 (웹팩은 바벨 로더가 만든 코드를 만나면 core-js를 찾을 것이기 때문에 추가 설치가 필요함)
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: "babel-loader", // 바벨 로더를 추가한다
+//       },
+//     ],
+//   },
+// }
