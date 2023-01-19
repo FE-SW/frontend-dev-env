32722fd8344aa22943399af564c0823c36113b23
FE-SW <ingal777@naver.com>
2023-01-20 04:50:42 +0900

    Feat: eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,30 @@
-// TODO: eslint를 구성해 보세요. 
+// TODO: eslint를 구성해 보세요.
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
+
+// module.exports = {
+//   extends: [
+//     "eslint:recommended", // 미리 설정된 규칙 세트을 사용한다
+//   ],
+// //   rules: {
+// //     "no-unexpected-multiline": "error",
+// //     "no-extra-semi": "error",
+// //   },
+// };
+
+
+//npx eslint app.js
+//npx eslint --init: exlint 자동설정
